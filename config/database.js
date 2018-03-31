ff9d64d2f7972341995e19351d04de19b3364f95
Karina Dorantes <danirak21@gmail.com>
2018-03-30 20:05:54 -0400

    Delete database.js

--- config/database.js
+++
@@ -1,6 +0,0 @@
-// config/database.js
-module.exports = {
-
-    'url' : 'mongodb://demo:demo@ds125489.mlab.com:25489/roulette' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-
-};
